f49a1b57504a01b0009739dc01583bcaddb8ff3b
giapbnv <nguyen.van.giapb@sun-asterisk.com>
2019-04-16 16:24:27 +0700

    chapter 5: start training

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/home', to: "static_pages#home"
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
+  get '/login', to: "static_pages#login"
   resources :microposts
   resources :users
   root 'users#index'
